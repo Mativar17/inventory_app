83a4f24fe8c9a514e57be8eb60eed669116ebc7a
Mativar17 <matyvargasg14@gmail.com>
2022-02-24 16:56:46 -0300

    modified views

--- app/models/oficina.rb
+++ app/models/oficina.rb
@@ -1,2 +1,7 @@
 class Oficina < ApplicationRecord
+    validates :name, presence: true
+    validates :sku, presence: true
+    validates :valor_neto_unitario, presence: true
+    validates :stock_incial, presence: true
+    validates :compra_mes, presence: true
 end
