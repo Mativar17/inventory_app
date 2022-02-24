6280b39ada0e4d8c969dd88a568e887c281e76e1
Mativar17 <matyvargasg14@gmail.com>
2022-02-24 18:04:28 -0300

    new changes to office model

--- app/models/oficina.rb
+++ app/models/oficina.rb
@@ -2,6 +2,5 @@ class Oficina < ApplicationRecord
     validates :name, presence: true
     validates :sku, presence: true
     validates :valor_neto_unitario, presence: true
-    validates :stock_incial, presence: true
     validates :compra_mes, presence: true
 end
