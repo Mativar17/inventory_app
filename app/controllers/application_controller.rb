56bf2a637c7d2ff3f032d1077e36848d46ccfb28
Mativar17 <matyvargasg14@gmail.com>
2022-04-16 21:58:47 -0400

    adding i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    around_action :set_locale
+
+    def set_locale
+        I18n.locale = :es
+    end
+    
+
 end
